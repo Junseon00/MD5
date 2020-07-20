a048bdca9adf25d7e7a5daf6760af0624737bc6f
Mullan <dumbo2019@daum.net>
2020-07-20 17:28:03 +0900

    search log prototype

--- app/src/main/java/com/example/myapplication/screen7camera.java
+++ app/src/main/java/com/example/myapplication/screen7camera.java
@@ -4,11 +4,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -52,7 +58,7 @@ public class screen7camera extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         iv= (ImageView)findViewById(R.id.iv);
         iv.setImageURI(data.getData());
-        //getData로 이미지 불러오기.
+        //getData로 이미지 URL불러오기.
 
         //서버로 보낼 수 있게 버튼 활성화
         send =(Button)findViewById(R.id.button10);
@@ -63,8 +69,45 @@ public class screen7camera extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //서버로 전송하는 코드 넣기;
+                try{
+
+                    ////날짜, 약 이름, 찍은 이미지
+                    String date = returnDate();
+                    String name = "default pill name";
+                    Bitmap image = (Bitmap)data.getExtras().get("data");
+
+
+                    //전송 retrofit코드 (& String에 name 변경)
+
+                    //검색 결과 저장
+                    MyDatabaseOpenHelper helper = new MyDatabaseOpenHelper(screen7camera.this);
+                    SQLiteDatabase db = helper.getWritableDatabase();
+
+                    //table만듦 검색 날짜, 약 이름, 이미지,
+                    String createsql = "CREATE TABLE  IF NOT EXISTS searchlog" +" (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            "date TEXT," +
+                            "name TEXT," +
+                            "image BLOB);";;
+
+                    db.execSQL(createsql);
+
+
+                    db.execSQL("insert into searchlog (date, name, image) values(?,?,?)" ,new Object[]{date,name,image}  );
+                    db.close();
+
+
+                }
+                catch(Exception e1){
+                    Log.d("udb 서버에 send","서버에 사진을 보내는 try- catch문에 에러 발생");
+                }
+
             }
-        });
+        }
+        //------------------------------------------------------------------------------------------
+
+
+
+        );
 
         //사진 찍기는 이제 다시 찍기로 설정
         takephoto =(Button)findViewById(R.id.button9);
@@ -74,17 +117,21 @@ public class screen7camera extends AppCompatActivity {
 
     }
 
-    //서버로 이미지를 보내는 코드
-    public void sendImage(){
-        Retrofit retrofit;
-        retroAPI apiService;
 
+public String returnDate(){
 
+    // 현재시간을 msec 으로 구한다.
+    long now = System.currentTimeMillis();
+    // 현재시간을 date2 변수에 저장한다.
+    Date date2 = new Date(now);
+    // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+    // String 형식으로 format을 변환해 time에 저장.
+    String time = sdfNow.format(date2);
 
+    return  time;
 
-    }
-
-
+}
 
 
 
