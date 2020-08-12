6ee13b097934ed0e5f310ad7d74140aa64d8a1cc
Mullan <dumbo2019@daum.net>
2020-08-12 20:00:31 +0900

    calendar

--- app/src/main/java/com/example/myapplication/screen8calender.java
+++ app/src/main/java/com/example/myapplication/screen8calender.java
@@ -3,10 +3,24 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.CalendarView;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 
 public class screen8calender extends AppCompatActivity {
 
@@ -18,26 +32,57 @@ public class screen8calender extends AppCompatActivity {
         setContentView(R.layout.activity_screen8calender);
         calendar = findViewById(R.id.calendarView);
 
-        calendar.addDecorators(
-                new SundayDecorator(),
-                new SaturdayDecorator(),
-                oneDayDecorator);
+        //특정 날짜에 점 표시
+        String[] result = {"2020,08,18","2020,08,19","2020,09,11"};
+        new ApiSimulator(result).executeOnExecutor(Executors.newSingleThreadExecutor());
 
-        //클릭이벤트
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
 
-            }
-        });
-
-        int beforeYear = Integer.parseInt(getYear(String.valueOf(befor)))
-
+//        //클릭이벤트
+//        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+//
+//            }
+//        });
+//
+//        int beforeYear = Integer.parseInt(getYear(String.valueOf(befor)))
 
 
     }
 
 
+    //특정 날짜에 효과 표시
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MONTH, -2);
+            ArrayList<CalendarDay> dates = new ArrayList<>();
+            for (int i = 0; i < 30; i++) {
+                CalendarDay day = CalendarDay.from(calendar);
+                dates.add(day);
+                calendar.add(Calendar.DATE, 5);
+            }
+
+            return dates;
+        }
+
+        @Override
+        protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+            super.onPostExecute(calendarDays);
+
+            if (isFinishing()) {
+                return;
+            }
+
+            calendar.addDecorator(new EventDecorator(Color.RED, calendarDays,screen8calender.this));
+        }
+    }
 
 }
-
