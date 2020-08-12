771dc65664998cfd3e405615aacdae7e9b27c043
Mullan <dumbo2019@daum.net>
2020-08-12 18:44:33 +0900

    material calendar 추가

--- app/src/main/java/com/example/myapplication/screen8calender.java
+++ app/src/main/java/com/example/myapplication/screen8calender.java
@@ -1,17 +1,43 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class screen8calender extends AppCompatActivity {
+
+    MaterialCalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen8calender);
+        calendar = findViewById(R.id.calendarView);
+
+        calendar.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                oneDayDecorator);
+
+        //클릭이벤트
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
+
+        int beforeYear = Integer.parseInt(getYear(String.valueOf(befor)))
+
+
+
     }
 
 
 
 }
+
