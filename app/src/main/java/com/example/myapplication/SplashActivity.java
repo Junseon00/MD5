563b2eb2a0223eb8af2ee6a72dc0dca86b337e8f
blorange28 <psejjh@naver.com>
2020-08-31 18:56:50 +0900

    스플래시 화면&디자인

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -1,16 +1,24 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static java.lang.Thread.sleep;
+
 public class SplashActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, screen1.class));
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finish();
     }
 
 
